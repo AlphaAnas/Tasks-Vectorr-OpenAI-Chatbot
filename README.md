e78e8d5641362c8dcb04d42f02715cac6d7301c1
AlphaAnas <96944434+AlphaAnas@users.noreply.github.com>
2025-10-13 21:01:48 +0500

    task1a done

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tasks-Vectorr-OpenAI-Chatbot
+
+LLM Calls
+    1a. Using Flask or FASTAPI for simple LLM (openai) calls
+    1b. Conversational chatbot in terminal:
+        Use case: Support agent for Nestle Pakistan
+    1c. Image generation using OpenAI
+        Use case: Allow text input with text and image output
+    1d. Image editing using OpenAI
+        Use case: Allow text + image input with text+ image output
+    1e. Function calling:
+        (Tasks 1c and 1d could be done using function calling as well)
+        Use case:  conversational chatbot which knows the current time
---
+++ task_1a.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv("OPENAI_API_KEY")
+
+
+app = FastAPI()
+client = OpenAI()
+
+
+response = client.responses.create(
+    model="gpt-4.1",
+    input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+
+@app.post("/call_chatbot")
+async def call_chatbot(request: PromptRequest):
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=str(request.prompt)
+    )
+    # print("Response:", response)
+    return {"response": response.output_text}
+
+
+
+
+
+
